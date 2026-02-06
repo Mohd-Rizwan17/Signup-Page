d1cd59e411ca0855446d43c2d3e04d78faf657c6
Mohd Rizwan <khanrizwan78617@gmail.com>
2026-02-06 19:22:42 +0530

    update css file

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -6,11 +6,12 @@ function HomePage() {
   const navigate = useNavigate();
 
   return (
-    <div className="homeWrap">
-      <h1>Welcome 👋</h1>
-      <p>Please login to continue</p>
-
-      <button onClick={() => navigate("/login")}>Click Here</button>
+    <div className="home-page">
+      <div className="home-card">
+        <h1>Welcome 👋</h1>
+        <p>Please login to continue</p>
+        <button onClick={() => navigate("/login")}>Click Here</button>
+      </div>
     </div>
   );
 }
